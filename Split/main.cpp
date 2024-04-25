d3b2b82129d31657d4b7f9073e30110cf76af81c
MAA91 <Sanya13082007@gmail.com>
2024-04-25 23:10:43 +0300

    Split done

---
+++ Split/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	setlocale(0, "");
+	srand(time(NULL));
+
+	int size_A = 10;
+	int size_even{}, size_odd{};
+	int* A = new int[size_A];
+
+	for (int i = 0; i < size_A; i++)
+	{
+		*(A + i) = rand() % 100;
+		cout << *(A + i) << "\t";
+		if (*(A + i) % 2 == 0)
+			size_even++;
+		else
+			size_odd++;
+	}
+	cout << endl;
+
+	int* even = new int[size_even];
+	int* odd = new int[size_odd];
+	int even_t{}, odd_t{};
+	for (int i = 0; i < size_A; i++)
+		if (*(A + i) % 2 == 0)
+			*(even + even_t++) = *(A + i);
+		else
+			*(odd + odd_t++) = *(A + i);
+
+	for (int i = 0; i < size_even; i++)
+		cout << *(even + i) << "\t";
+	cout << endl;
+	for (int i = 0; i < size_odd; i++)
+		cout << *(odd + i) << "\t";
+	cout << endl;
+}
