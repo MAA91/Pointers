8e5e72953bf4c7016459852f3f12a13e30ae9ad7
MAA91 <Sanya13082007@gmail.com>
2024-04-26 21:19:42 +0300

    Split in Function

--- Split/main.cpp
+++ Split/main.cpp
@@ -1,47 +1,43 @@
-#include <iostream>
+#include "stdafx.h"
+#include "BaseFunction.h"
+#include "BaseFunction.cpp"
 
-using namespace std;
+void Split(int* A, int size_A, int* even, int* odd);
 
 void main()
 {
 	setlocale(0, "");
 	srand(time(NULL));
 
-	int size_A = 10;
-	int size_even{}, size_odd{};
+	unsigned int size_A = 10;
 	int* A = new int[size_A];
+	FillRand(A, size_A, 0, 10);
+	Print(A, size_A);
 
+	int size_even{}, size_odd{};
 	for (int i = 0; i < size_A; i++)
-	{
-		*(A + i) = rand() % 100;
-		cout << *(A + i) << "\t";
-		if (*(A + i) % 2 == 0)
-			size_even++;
-		else
-			size_odd++;
-	}
-	cout << endl;
-
+		*(A + i) % 2 == 0 ? size_even++ : size_odd++;
+	
 	int* even = new int[size_even];
 	int* odd = new int[size_odd];
-	int even_t{}, odd_t{};
-	for (int i = 0; i < size_A; i++)
-		if (*(A + i) % 2 == 0)
-			*(even + even_t++) = *(A + i);
-		else
-			*(odd + odd_t++) = *(A + i);
-
+	Split(A, size_A, even, odd);
+	
 	cout << "Even: ";
-	for (int i = 0; i < size_even; i++)
-		cout << *(even + i) << "\t";
-	cout << endl;
+	Print(even, size_even);
 
 	cout << "Odd: ";
-	for (int i = 0; i < size_odd; i++)
-		cout << *(odd + i) << "\t";
-	cout << endl;
+	Print(odd, size_odd);
 
-	delete A;
-	delete even;
-	delete odd;
+	delete[] A;
+	delete[] even;
+	delete[] odd;
+}
+
+void Split(int* A, int size_A, int* even, int* odd)
+{
+	int* p_even = even, * p_odd = odd;
+
+	for (int i = 0; i < size_A; i++)
+		*(A + i) % 2 == 0 ? *(p_even++) = *(A + i) :
+		*(p_odd++) = *(A + i);
 }
