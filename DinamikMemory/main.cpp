2c87b33f675a31ca26b44ae2c83bd30bfed97ae8
MAA91 <Sanya13082007@gmail.com>
2024-04-29 20:26:42 +0300

    PushBack done

--- DinamikMemory/main.cpp
+++ DinamikMemory/main.cpp
@@ -2,6 +2,7 @@
 #include "BaseFunction.h"
 #include "BaseFunction.cpp"
 
+int* PushBack(int* A, int& size_A, int element);
 
 void main()
 {
@@ -14,18 +15,29 @@ void main()
 	FillRand(A, size_A, 0, 10);
 	Print(A, size_A);
 
-	int veleu{};
+	A = PushBack(A, size_A, 5);
+	/*int veleu{};
 	cout << "¬ведите значение переменной дл€ добавлени€ еЄ "
 		<< "в массив: "; cin >> veleu;
 
 	int* Buf = A;
 	A = new int[size_A + 1];
-
 	for (int i = 0; i < size_A; i++)
 		*(A + i) = *(Buf + i);
 	delete[] Buf;
-	*(A + size_A++) = veleu;
+	*(A + size_A++) = veleu;*/
 	Print(A, size_A);
 
 	delete[] A;
+}
+
+int* PushBack(int* A, int& size_A, int element)
+{
+	int* Buf = new int[size_A + 1];
+	for (int i = 0; i < size_A; i++)
+		*(Buf + i) = *(A + i);
+	*(Buf + size_A) = element;
+	size_A++;
+	delete[] A;
+	return Buf;
 }
