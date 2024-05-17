96c7bce7f3b92afd2940c00f08da4adcb7262267
MAA91 <Sanya13082007@gmail.com>
2024-05-17 19:23:41 +0300

    Nothing New

--- DinamikMemory/BaseFunction.h
+++ DinamikMemory/BaseFunction.h
@@ -2,10 +2,7 @@
 #pragma once
 #include "stdafx.h"
 
-template <typename T>
-void Print(T A[], int size); 
-template <typename T>
-void Print(T** A, int ROW, int COL);
+template <typename T> void Print(T A[], int size); 
+template <typename T> void Print(T** A, int ROW, int COL);
 
-template <typename T>
-void Search(T A[]);
+template <typename T> void Search(T A[]);
